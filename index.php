3c006a1e82e3bcbc5b3f06948a996c7a3f2bee13
bogdan <litvinenkob16@gmail.com>
2023-12-25 19:54:25 +0200

    Сохранил прогресс

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-    echo 'hello world';
+
+define('VG_ACCESS', true);
+
+
+header('Content-Type: text/html;charset=utf-8');
+session_start();
+
+require_once 'config.php';
+require_once 'core/base/settings/internal_settings.php';
+
+echo 'hello';
